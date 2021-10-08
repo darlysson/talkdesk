1d6ea011443fb78e4806d9240349d030c46b60ba
darlyssonalves <dalves@alter-solutions.com>
2021-10-08 10:24:22 +0100

    Code improvement

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 
-import styles from './styles.module.scss'
 import { Label } from '../../../types'
+
+import styles from './styles.module.scss'
 
 interface sidebarProps {
   labels: Label[]
@@ -13,25 +14,21 @@ export default function Sidebar({ labels, filteredData }: sidebarProps) {
   const [activeLabel, setActiveLabel] = useState('All')
 
   function handleSelectFilter(label: { label: string }) {
-    for (let i = 0; i < labels.length; i++) {
-      if (label.label === 'Product') {
-        filteredData('All')
-      } else if (label.label === 'All') {
-        filteredData('')
-      } else if (label.label === labels[i].label) {
-        filteredData(labels[i].label)
-      }
-
-      if (labels[i].label == label.label) {
-        setActiveLabel(label.label)
-      }
+    if (label.label === 'Product') {
+      filteredData('All')
+    } else if (label.label === 'All') {
+      filteredData('')
+    } else {
+      filteredData(label.label)
     }
+
+    setActiveLabel(label.label)
   }
 
   return (
     <aside className={styles.sidebar}>
       <ul>
-        {labels.map((label: { label: string, slug: string }) => {
+        {labels.map((label: Label) => {
           return (
             <li key={label.slug}>
               <Link href="/">
