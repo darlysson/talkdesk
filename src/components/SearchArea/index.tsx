b711363ac619f819d320399fcc7e3ce1096c1f1a
darlyssonalves <dalves@alter-solutions.com>
2021-10-08 10:22:04 +0100

    Code improvement

--- src/components/SearchArea/index.tsx
+++ src/components/SearchArea/index.tsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import styles from './styles.module.scss'
 
 interface searchAreaProps {
   inputContent: (value: string) => void;
-  searchItems: (e: React.ChangeEvent<EventTarget>) => void;
+  searchItems: (e: ChangeEvent<EventTarget>) => void;
 }
 
 export function SearchArea({ inputContent, searchItems }: searchAreaProps) {
   return (
     <form
-      onSubmit={(e: React.ChangeEvent<EventTarget>) => searchItems(e)}
+      onSubmit={(e: ChangeEvent<EventTarget>) => searchItems(e)}
       className={styles.inputContainer}>
 
       <input
         type='text'
         placeholder='Type to search'
+        // value={e.target.value}
         onChange={(e) => inputContent(e.target.value)}
       />
       <button
         type='button'
-        onClick={(e: React.ChangeEvent<EventTarget>) => searchItems(e)}>
+        onClick={(e: ChangeEvent<EventTarget>) => searchItems(e)}>
 
         Search
       </button>
